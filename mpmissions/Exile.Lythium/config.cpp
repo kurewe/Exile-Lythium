f4d878888f623c0fc5a1b22e89264c52b5f46a82
kurewe <kurewe@gmail.com>
2017-05-27 20:08:48 -0500

    Made Lythium it's own environment

--- mpmissions/Exile.Lythium/config.cpp
+++ mpmissions/Exile.Lythium/config.cpp
@@ -3260,18 +3260,18 @@ class CfgExileCustomCode
 
 class CfgExileEnvironment
 {
-	class Altis 
+	class Lythium 
 	{
 		class FireFlies
 		{
 			// 1 = enabled, 0 = disabled
-			enable = 0;
+			enable = 1;
 
 			// At this hour fire flies begin to spawn
 			startHour = 18;
 
 			// At this hour fire flies stop spawning
-			endHour = 4;
+			endHour = 6;
 		};
 
 		class Anomalies
@@ -3304,7 +3304,33 @@ class CfgExileEnvironment
 		class Radiation 
 		{
 			// 1 = enabled, 0 = disabled
-			enable = 1;
+			enable = 0;
+
+			/*
+				Defines contaminated zones in a specific map. 
+				You can define multiple zones per map. The format
+				of the zones is:
+
+				[Position ASL(!), Full Radiation Radius, Maximum Radius]
+
+				The radius works as follows:
+
+	            |-------------------------------------------------------|
+	                                Maximum Radius
+				
+				|------------------------|
+				  Full Radiation Radius   
+
+				Within the full radiation radius, radiation factor is
+				always at a maximum. Outside of this, it lowers down
+				to no radiation smoothly.
+
+				Radiation:
+
+	            |------------------------|------------------------------|
+	            1        1      1        1     0.75    0.5     0.25    0
+			*/
+			contaminatedZones[] = {};
 		};
 
 		class Temperature
@@ -3329,113 +3355,8 @@ class CfgExileEnvironment
 			water = -5;
 		};
 	};
-
-	class Namalsk: Altis 
-	{
-		class FireFlies: FireFlies
-		{
-			enable = 1;
-		};
-
-		class Anomalies: Anomalies
-		{
-			enable = 1;
-		};
-
-		class Breathing: Breathing
-		{
-			enable = 1;
-		};
-
-		class Snow: Snow
-		{
-			enable = 1;
-			surfaces[] = {"#nam_snow"};
-		};
-
-		class Radiation: Radiation
-		{
-			enable = 1;
-			contaminatedZones[] = 
-			{
-				{{3960.14,	8454.75,	152.862}, 	80, 	140},	// Object A1
-				{{4974.70,	6632.82,	4.74293}, 	40, 	150},	// Object A2
-				{{6487.92,	9302.03,	36.0014}, 	60, 	110}	// Sebjan Chemical Factory
-			};
-		};
-
-		class Temperature: Temperature
-		{
-			daytimeTemperature[] = {-2.00,-1.77,-1.12,-0.10,1.24,2.78,4.40,6.00,7.46,8.65,9.50,9.90,9.90,9.50,8.65,7.46,6.00,4.40,2.78,1.24,-0.10,-1.12,-1.77,-2.00,-2.00};
-		};
-	};
-
-	class Tanoa: Altis
-	{
-		class FireFlies: FireFlies
-		{
-			enable = 0;
-		};
-
-		class Anomalies: Anomalies
-		{
-			enable = 0;
-		};
-
-		class Breathing: Breathing
-		{
-			enable = 0;
-		};
-
-		class Snow: Snow
-		{
-			enable = 0;
-		};
-
-		class Radiation: Radiation
-		{
-			enable = 1;
-		};
-
-		class Temperature: Temperature
-		{
-			daytimeTemperature[] = {15.93,16.89,18.42,20.40,22.68,25.10,27.48,29.63,31.40,32.66,33.32,33.80,33.80,33.32,32.66,31.40,29.63,27.48,25.10,22.68,20.40,18.42,16.89,15.93,15.93};
-		};
-	};
-
-	class Lythium: Altis
-	{
-		class FireFlies: FireFlies
-		{
-			enable = 0;
-		};
-
-		class Anomalies: Anomalies
-		{
-			enable = 0;
-		};
-
-		class Breathing: Breathing
-		{
-			enable = 0;
-		};
-
-		class Snow: Snow
-		{
-			enable = 0;
-		};
-
-		class Radiation: Radiation
-		{
-			enable = 1;
-		};
-
-		class Temperature: Temperature
-		{
-			daytimeTemperature[] = {15.93,16.89,18.42,20.40,22.68,25.10,27.48,29.63,31.40,32.66,33.32,33.80,33.80,33.32,32.66,31.40,29.63,27.48,25.10,22.68,20.40,18.42,16.89,15.93,15.93};
-		};
-	};
 };
+
 class CfgExileHUD
 {
 	class ShortItemNames
